44d01fc2741ec030fb2ccad08602c18a809418a1
lenovo <maraalison9255@gmail.com>
2023-11-15 13:03:12 -0600

    Cambio de URL

--- CoolTesters923/src/main/java/practicaSel/PruebaSelenium2.java
+++ CoolTesters923/src/main/java/practicaSel/PruebaSelenium2.java
@@ -1,10 +1,13 @@
 package practicaSel;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PruebaSelenium2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.cooltesters.com");
 	}
 
 }
